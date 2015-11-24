5a63f9eff2a48ce1a19ad0d5d78fb275689e2735
Szasza <szasza@szasza.info>
2015-11-25 03:02:17 +1100

    initial version

---
+++ scripts/dep.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Adding MySQL repository
+gpg --keyserver keys.gnupg.net --recv-keys 5072E1F5
+
+touch /etc/apt/sources.list.d/mysql.list
+
+echo -e "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" >> /etc/apt/sources.list.d/mysql.list
+
+# Adding base packages
+apt-get -y install software-properties-common python-software-properties
+
+# Adding PHP repository
+add-apt-repository ppa:ondrej/php5-5.6
+
+# Repo update and package upgrade
+apt-get -y update
+apt-get -y ugrade
+
+# Setting MySQL root password to empty string
+export DEBIAN_FRONTEND="noninteractive"
+
+# Installing all the fun things
+apt-get -y --force-yes install mysql-server mysql-client apache2 php5 php5-mysql php5-mcrypt php5-curl
+
+# Adding the apache user to the vagrant group, so /vagrant can be the docroot
+adduser www-data vagrant
+
+# Adding /vagrant as docroot
+cat <<EOF > /etc/apache2/sites-available/vagrant.conf
+
+<VirtualHost *:80>
+    DocumentRoot /vagrant
+    <Directory /vagrant/>
+        Options -Indexes +FollowSymLinks +MultiViews
+        AllowOverride All
+        Order allow,deny
+        allow from all
+        Require all granted
+    </Directory>
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+rm -rf /etc/apache2/sites-enabled/*
+
+ln -s /etc/apache2/sites-available/vagrant.conf /etc/apache2/sites-enabled/vagrant.conf
---
+++ scripts/vagrant.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Vagrant specific
+date > /etc/vagrant_box_build_time
+
+# Installing vagrant keys
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# Customize the message of the day
+echo 'All code is guilty until proven innocent.' > /etc/motd
+
+# Add passwordless sudo for vagrant
+echo -e '\nvagrant ALL=(ALL) NOPASSWD: ALL\n' >> /etc/sudoers
